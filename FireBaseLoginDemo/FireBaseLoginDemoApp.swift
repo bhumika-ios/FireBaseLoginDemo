974e459c7e3ad7eeb13a3a81058a60dd43854068
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-11-25 16:56:14 +0530

    Initial Commit

---
+++ FireBaseLoginDemo/FireBaseLoginDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseLoginDemoApp.swift
+//  FireBaseLoginDemo
+//
+//  Created by Bhumika Patel on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseLoginDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
