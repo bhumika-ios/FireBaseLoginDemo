0e0299e4890b2bd4eb7ab1581e32ee83abad7293
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-11-28 17:28:15 +0530

    otp for verfication screen

---
+++ FireBaseLoginDemo/Verification.swift
@@ -0,0 +1,18 @@
+//
+//  Verification.swift
+//  FireBaseLoginDemo
+//
+//  Created by Bhumika Patel on 27/11/23.
+//
+
+import SwiftUI
+
+struct Verification: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Verification()
+}
