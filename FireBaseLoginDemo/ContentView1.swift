79bedf1508cd72c411ff85ad51d05bc47f9c63c6
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2023-11-29 16:54:30 +0530

    add verifivation

---
+++ FireBaseLoginDemo/ContentView1.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView1.swift
+//  FireBaseLoginDemo
+//
+//  Created by Bhumika Patel on 29/11/23.
+//
+
+import SwiftUI
+
+struct ContentView1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView1()
+}
---
+++ FireBaseLoginDemo/Login1.swift
@@ -0,0 +1,18 @@
+//
+//  Login1.swift
+//  FireBaseLoginDemo
+//
+//  Created by Bhumika Patel on 28/11/23.
+//
+
+import SwiftUI
+
+struct Login1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login1()
+}
